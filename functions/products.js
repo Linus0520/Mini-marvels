b91598eed61014d1247182f955abf3762b4bbaee
Zhen Wang <zhenwang@ZhendeAir.home>
2023-06-06 22:55:03 -0700

    update

--- functions/products.js
+++ functions/products.js
@@ -9,7 +9,7 @@ exports.handler = async function () {
   try {
     const response = await airtable.list({ maxRecords: 200 });
 
-    const products = response.records.map((product) => {
+    const products = response.records?.map((product) => {
       const { id, fields } = product;
       const {
         name,
